c77201362c334b915335641a25db16560371d54c
TechDragonXZ <88261535+TechDragonXZ@users.noreply.github.com>
2026-03-09 19:13:35 -0400

    Reapply "Reapply "Merge remote-tracking branch 'shiny/DayNight' into Upgraded-Emerald-V2""
    This reverts commit 52a16472ca36018cd25cfc1ca646dbf4fafdfbaa.

--- src/pokemon.c
+++ src/pokemon.c
@@ -48,6 +48,8 @@
 #include "constants/songs.h"
 #include "constants/trainers.h"
 #include "constants/union_room.h"
+#include "day_night.h"
+#include "constants/day_night.h"
 
 #define DAY_EVO_HOUR_BEGIN       12
 #define DAY_EVO_HOUR_END         HOURS_PER_DAY
--- src/sprite.c
+++ src/sprite.c
@@ -2,6 +2,7 @@
 #include "sprite.h"
 #include "main.h"
 #include "palette.h"
+#include "day_night.h"
 
 #define MAX_SPRITE_COPY_REQUESTS 64
 
@@ -91,7 +92,7 @@ static void GetAffineAnimFrame(u8 matrixNum, struct Sprite *sprite, struct Affin
 static void ApplyAffineAnimFrame(u8 matrixNum, struct AffineAnimFrameCmd *frameCmd);
 static u8 IndexOfSpriteTileTag(u16 tag);
 static void AllocSpriteTileRange(u16 tag, u16 start, u16 count);
-static void DoLoadSpritePalette(const u16 *src, u16 paletteOffset);
+static void DoLoadSpritePalette(const u16 *src, u16 paletteOffset, bool32 isDayNight);
 static void UpdateSpriteMatrixAnchorPos(struct Sprite *, s32, s32);
 
 typedef void (*AnimFunc)(struct Sprite *);
@@ -1601,7 +1602,7 @@ void FreeAllSpritePalettes(void)
         sSpritePaletteTags[i] = TAG_NONE;
 }
 
-u8 LoadSpritePalette(const struct SpritePalette *palette)
+static u8 LoadSpritePalette_HandleDayNight(const struct SpritePalette *palette, bool32 isDayNight)
 {
     u8 index = IndexOfSpritePaletteTag(palette->tag);
 
@@ -1617,9 +1618,14 @@ u8 LoadSpritePalette(const struct SpritePalette *palette)
     else
     {
         sSpritePaletteTags[index] = palette->tag;
-        DoLoadSpritePalette(palette->data, PLTT_ID(index));
+        DoLoadSpritePalette(palette->data, PLTT_ID(index), isDayNight);
         return index;
     }
+}
+
+u8 LoadSpritePalette(const struct SpritePalette *palette)
+{
+    return LoadSpritePalette_HandleDayNight(palette, FALSE);
 }
 
 void LoadSpritePalettes(const struct SpritePalette *palettes)
@@ -1633,13 +1639,14 @@ void LoadSpritePalettes(const struct SpritePalette *palettes)
 u8 LoadSpritePaletteInSlot(const struct SpritePalette *palette, u8 paletteNum) {
     paletteNum = min(15, paletteNum);
     sSpritePaletteTags[paletteNum] = palette->tag;
-    DoLoadSpritePalette(palette->data, paletteNum * 16);
+    DoLoadSpritePalette(palette->data, paletteNum * 16, FALSE);
     return paletteNum;
 }
 
-void DoLoadSpritePalette(const u16 *src, u16 paletteOffset)
+static void DoLoadSpritePalette(const u16 *src, u16 paletteOffset, bool32 isDayNight)
 {
     LoadPalette(src, OBJ_PLTT_OFFSET + paletteOffset, PLTT_SIZE_4BPP);
+    LoadPalette_HandleDayNight(src, OBJ_PLTT_OFFSET + paletteOffset, PLTT_SIZE_4BPP, isDayNight);
 }
 
 u8 AllocSpritePalette(u16 tag)
@@ -1778,4 +1785,9 @@ bool8 AddSubspritesToOamBuffer(struct Sprite *sprite, struct OamData *destOam, u
     }
 
     return 0;
+}
+
+u8 LoadSpritePaletteDayNight(const struct SpritePalette *palette)
+{
+    return LoadSpritePalette_HandleDayNight(palette, TRUE);
 }
